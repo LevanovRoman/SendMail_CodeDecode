e8060295a5a8a173033adaea80f0bfdbe2471435
levanovroman <levanovroman2016@yandex.ru>
2024-05-17 16:09:54 +0300

    time

--- src/main/java/com/example/sendmailcodeDecode/SendMailCodeDecodeApplication.java
+++ src/main/java/com/example/sendmailcodeDecode/SendMailCodeDecodeApplication.java
@@ -1,30 +1,95 @@
 package com.example.sendmailcodeDecode;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 
 @SpringBootApplication
 public class SendMailCodeDecodeApplication {
 
-	public static void main(String[] args) {
+	@Autowired
+	private JavaMailSender javaMailSender;
 
-		SpringApplication.run(SendMailCodeDecodeApplication.class, args);
+	@Value("$(spring.mail.username)")
+	private String fromEmailId;
 
-//		System.out.println("START");
+	LocalDateTime startDateTime;
+
+	public void sendEmail(String recipient, String body, String subject){
+
+		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+		simpleMailMessage.setFrom(fromEmailId);
+		simpleMailMessage.setTo(recipient);
+		simpleMailMessage.setText(body);
+		simpleMailMessage.setSubject(subject);
+
+		javaMailSender.send(simpleMailMessage);
 
 	}
 
-//	@PreDestroy
-//	public void onExit() {
-//		System.out.println("###STOPing###");
-//		try {
-//			Thread.sleep(5 * 1000);
-//		} catch (InterruptedException e) {
-//			System.out.println("ERR");;
-//		}
-//		System.out.println("###STOP FROM THE LIFECYCLE###");
+	public static void main(String[] args) throws AWTException, IOException {
+
+		SpringApplication.run(SendMailCodeDecodeApplication.class, args);
+
+		System.out.println("START");
+
+//		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+//		Rectangle screenRectangle = new Rectangle(screenSize);
+//		Robot robot = new Robot();
+//		BufferedImage image = robot.createScreenCapture(screenRectangle);
+//		ImageIO.write(image, "png", new File("screenShot.png"));
+
+	}
+	@PostConstruct
+	public void onStart() throws Exception {
+		System.out.println("BEGIN");
+		startDateTime = LocalDateTime.now();
+		LocalDate dateNow = LocalDate.now();
+		LocalTime timeNow = LocalTime.now();
+		sendEmail("levanovroman2016@yandex.ru", "Компьютер включен:\n дата:" + dateNow +
+				"\nвремя:  " + timeNow, "Test Subject");
+
+//		givenMainScreen_whenTakeScreenshot_thenSaveToFile();
+	}
+
+	@PreDestroy
+	public void onExit() throws Exception {
+		System.out.println("###STOPing###");
+
+		LocalDateTime finishDateTime = LocalDateTime.now();
+
+		long diffHours = ChronoUnit.HOURS.between(startDateTime, finishDateTime);
+		long diffMinutes = ChronoUnit.MINUTES.between(startDateTime, finishDateTime);
+
+		String body = "Компьютер выключен\nВремя работы:  " + diffHours +" часов  " + (diffMinutes - diffHours * 60) + " минут";
+
+		sendEmail("levanovroman2016@yandex.ru", body, "Test Subject");
+//		givenMainScreen_whenTakeScreenshot_thenSaveToFile();
+	}
+
+//	public void givenMainScreen_whenTakeScreenshot_thenSaveToFile() throws Exception {
+//		Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+//		BufferedImage capture = new Robot().createScreenCapture(screenRect);
+//
+//		File imageFile = new File("single-screen.bmp");
+//		ImageIO.write(capture, "bmp", imageFile );
 //	}
 
 }
